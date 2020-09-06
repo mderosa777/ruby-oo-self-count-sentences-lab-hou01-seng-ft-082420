cf9475c2bf7452367c6dbca8690c2c66fa03f617
mderosa777 <meggsnbacon777@gmail.com>
2020-09-05 23:04:28 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
+    if self.include?(".")
+      return true
+    else 
+      return false
+    
+    end
     
   end
 
   def question?
+    if self.include?("?")
+        true
+    else 
+       false 
+    end
 
   end
 
   def exclamation?
-
+    if self.include?("!")
+      true
+      else
+      false
+      end 
   end
 
   def count_sentences
+    self.split(/[.?!] /).count 
 
   end
 end
